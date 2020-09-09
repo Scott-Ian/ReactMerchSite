e3809ba65304b85550c5160ccd805e38ab4073b5
Ian Scott <chesnekov@gmail.com>
2020-09-09 10:19:59 -0700

    initial commit, add react template, add components subfolder in src directory
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

---
+++ merch-site/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div class="container">
+        <Header />
+
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
